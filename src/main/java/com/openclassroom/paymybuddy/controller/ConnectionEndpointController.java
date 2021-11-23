b4fe41e141dcd877d70a787cf92d10d7cfe49ab0
Yves <sacmusicien6735xbox@gmail.com>
2021-11-23 15:45:47 +0100

    Ajouts de l'annotation RestController sur deux classes

--- src/main/java/com/openclassroom/paymybuddy/controller/ConnectionEndpointController.java
+++ src/main/java/com/openclassroom/paymybuddy/controller/ConnectionEndpointController.java
@@ -12,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping(value = "/connection")
 public class ConnectionEndpointController {
 
